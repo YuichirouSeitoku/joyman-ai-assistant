c9616e083b96f373b230d9a25998223c5c0e0241
YuichirouSeitoku <kazeso.kazewo@gmail.com>
2024-11-24 14:08:20 +0900

    llamaが固定の質問に返答を返してくれる処理の実装(#1)

---
+++ src/index.d.ts
@@ -0,0 +1,12 @@
+export type Bindings = {
+  AI: any
+}
+
+export type Answer = {
+  response: string
+}
+
+export type Message = {
+  content: string
+  role: string
+}
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
+import { Ai } from '@cloudflare/ai'
+import { AiTextGenerationOutput } from '@cloudflare/ai/dist/ai/tasks/types/tasks'
 import { Hono } from 'hono'
 
-const app = new Hono()
+type Bindings = {
+  AI: any
+}
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+const app = new Hono<{ Bindings: Bindings }>()
+
+app.get('/ai', async (c) => {
+  const ai = new Ai(c.env.AI)
+  const answer:AiTextGenerationOutput = await ai.run('@cf/meta/llama-2-7b-chat-int8', {
+    messages: [
+      {
+        role: 'user',
+        content: `日本のお笑い芸人、ジョイマンの説明をしてください`
+      }
+    ]
+  })
+
+  return c.text(JSON.stringify(answer))
 })
 
 export default app
